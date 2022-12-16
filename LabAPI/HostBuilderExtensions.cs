1a9cfb6453acf84093e7fbb9ed6ff731c461a956
crjcodes <crjcodes@users.noreply.github.com>
2022-12-16 08:27:23 -0600

    switched to minimal api

--- LabAPI/HostBuilderExtensions.cs
+++ LabAPI/HostBuilderExtensions.cs
@@ -1,55 +1,41 @@
-﻿using LabAPI.Models;
+﻿//using LabAPI.Models;
 
-namespace LabAPI
-{
-    public static class HostBuilderExtensions
-    {
-        public static WebApplicationBuilder Configure(this WebApplicationBuilder builder)
-        {
-            // Load into the configuration the mock data
+//namespace LabAPI
+//{
+//    public static class HostBuilderExtensions
+//    {
+//        public static WebApplicationBuilder BuildWebServer(this WebApplicationBuilder builder)
+//        {
+//            // some scaffolded stuff that's rather lovely
 
-            builder.Configuration.AddJsonFile("mockdata.json");
+//            builder.Services.BuildServices();
+//            return builder;
+//        }
 
-            builder.Services.AddOptions();
-            builder.Services.Configure<List<FlattenedLabRecord>>(
-                builder.Configuration.GetSection("LabRecords"));
+//        public static IServiceCollection BuildServices(this IServiceCollection services)
+//        {
+//            services.AddControllers();
+//            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//            services.AddEndpointsApiExplorer();
+//            services.AddSwaggerGen();
 
+//            return services;
+//        }
 
-            return builder;
-        }
+//        public static WebApplication SetupApp(this WebApplication app)
+//        {
+//            // Configure the HTTP request pipeline.
+//            if (app.Environment.IsDevelopment())
+//            {
+//                app.UseSwagger();
+//                app.UseSwaggerUI();
+//            }
 
-        public static WebApplicationBuilder BuildWebServer(this WebApplicationBuilder builder)
-        {
-            // some scaffolded stuff that's rather lovely
+//            app.UseHttpsRedirection();
+//            app.UseAuthorization();
+//            app.MapControllers();
 
-            builder.Services.BuildServices();
-            return builder;
-        }
-
-        public static IServiceCollection BuildServices(this IServiceCollection services)
-        {
-            services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-
-            return services;
-        }
-
-        public static WebApplication SetupApp(this WebApplication app)
-        {
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseAuthorization();
-            app.MapControllers();
-
-            return app;
-        }
-    }
-}
+//            return app;
+//        }
+//    }
+//}
--- LabAPI/Program.cs
+++ LabAPI/Program.cs
@@ -1,12 +1,51 @@
 using LabAPI;
+using LabAPI.Models;
+
+//========================================================================
+// BUILDER ACTIONS
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configure();
-builder.BuildWebServer();
+// Load into the configuration the mock data
+
+// For this simple example, this approach works
+// In the real world, the data would be more voluminous and coming from
+// a database read, most likely
+
+builder.Configuration.AddJsonFile("mockdata.json");
+
+//========================================================================
+// BUILDER ACTIONS - SERVICE SETUP
+
+// if we were to move to a service, we would need to add logic here to inject
+// the mock data configuration using a pattern like Options/Configure<List<FlattendLab...
+// In the real world, patterns commonly used are a dbContext or a repo or even a data access service
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.SetupApp();
+//========================================================================
+// APP SETUP
+//
+// Instead of this more traditional controllers approach use the minimal APIs instead
 
-app.RunAsync();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// In the real world, this would be coming from a service -- data access, dbContext, or repo, etc
+
+var records = builder.Configuration.GetSection("LabRecords").Get<List<FlattenedLabRecord>>();
+
+//app.MapGet("/", () => "Hello");
+app.MapGet("/", () => { return records; });
+app.MapGet("/LabRecords", () => { return records; });
+app.MapGet("/LabNames", () => { return records.Select(r => r.Name).Distinct(); });
+
+app.Run();
+
+
